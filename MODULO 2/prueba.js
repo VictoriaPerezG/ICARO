07072b0333fe26df41ebaa7f364479f5dd19fc78
VictoriaPerezG <victoria.perez@est.fi.uncoma.edu.ar>
2025-05-16 08:23:50 -0300

    Agrego Modulo 2 y archivo js

---
+++ MODULO 2/prueba.js
@@ -0,0 +1,9 @@
+/*Crear un script que realice operaciones aritméticas básicas (suma, resta, multiplicación, división) 
+según los números ingresados por el usuario.
+Instrucciones:
+Pide dos números al usuario usando prompt, conviértelos a número con parseFloat() y almacenalos en dos 
+variables distintas.
+Realiza las operaciones aritméticas básicas y almacena los resultados.
+Muestra los resultados en un alert y en la consola.
+Tienes un momento para intentarlo y no olvides que puedes investigar en internet ¡Eso es parte de ser programador!
+ */
